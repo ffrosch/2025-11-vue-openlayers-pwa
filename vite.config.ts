bfc2560a4bec5fe19e4b1421d1fad8f49627c623
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-10-01 15:22:48 +0200

    chore: prompt for PWA update

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     vue(),
     tailwindcss(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       injectRegister: false,
 
       pwaAssets: {
